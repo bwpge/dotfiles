04cfaae68f8e008193526726299e97d347471f4d
bwpge <bwpge.dev@gmail.com>
2025-08-22 21:41:49 -0400

    Update system cleanup logic

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,6 +26,8 @@ _countdown() {
     echo
 }
 
+# TODO: update for other systems
+
 echo -e "${bold}[+] Updating system$nc"
 sudo apt update -y
 sudo apt upgrade -y
@@ -38,6 +40,10 @@ ansible-galaxy install -r requirements.yml
 
 echo -e "${bold}[+] Running ansible playbook$nc"
 ansible-playbook -K -i localhost, -c local main.yml
+
+echo -e "${bold}[+] Cleaning apt$nc"
+sudo apt clean -y
+sudo apt autoremove -y
 
 if [ $? = 0 ]; then
     echo -e "\n${bcyn}Some settings will not take effect until you logout$nc"
